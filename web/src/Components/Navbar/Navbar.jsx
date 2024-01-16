99c67b2a0efce836abfbcc9d7ac1d70d75e84c1d
bgt1717 <99308138+bgt1717@users.noreply.github.com>
2024-01-16 14:58:03 -0600

    navbar with ternary operators

--- web/src/Components/Navbar/Navbar.jsx
+++ web/src/Components/Navbar/Navbar.jsx
@@ -2,24 +2,28 @@ import React from 'react'
 import './Navbar.css'
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
+import { useState } from 'react'
 
 
 const Navbar = () => {
+    const[menu, setMenu] = useState("shop");
+
   return (
     <div className='navbar'>
       <div className="nav-logo">
         <img src={logo} alt="" />
-        <p>SHOPPER</p>
+        <p>STORE</p>
       </div>
       <ul className="nav-menu">
-        <li>Shop</li>
-        <li>Men</li>
-        <li>Women</li>
-        <li>Kids</li>
+        <li onClick={()=>{setMenu("shop")}}>Shop{menu==="shop"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("mens")}}>Men{menu==="mens"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("women")}}>Women{menu==="women"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("kids")}}>Kids{menu==="kids"?<hr/>:<></>}</li>
       </ul>
       <div className="nav-login-cart">
       <button>Login</button>
       <img src={cart_icon} alt="" />
+      <div className="nav-cart-count">0</div>
       </div>
     </div>
   )
