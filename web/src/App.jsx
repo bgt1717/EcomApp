4fd7fb3a988a19b90326595085c24da3f863cced
bgt1717 <99308138+bgt1717@users.noreply.github.com>
2024-01-16 15:23:08 -0600

    Routing added

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,16 +1,29 @@
 
-import './App.css'
-import Navbar from './Components/Navbar/Navbar'
+import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Shop from './Pages/Shop';
+import ShopCategory from './Pages/ShopCategory';
 
 function App() {
 
   return (
-    <>
-      <div>
+    <div>
+      <BrowserRouter>
       {/* Navbar mounted in app.jsx */}
-        <Navbar/> 
-      </div>
-    </>
+      <Navbar/> 
+      <Routes>
+        <Route path='/' element={<Shop/>}/>
+        {/* Routes for '/mens', '/womens', and '/kids' render the ShopCategory component with the respective category prop. */}
+        <Route path='/mens' element={<ShopCategory category="men"/>}/>
+        <Route path='/womens' element={<ShopCategory category="women"/>}/>
+        <Route path='/kids' element={<ShopCategory category="kid"/>}/>
+
+      </Routes>
+      </BrowserRouter>
+    </div>
+
+
 
   )
 }
